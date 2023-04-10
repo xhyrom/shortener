cfd674683a9a1cbef782dca6084d52f8a71f60c1
xHyroM <generalkubo@gmail.com>
2023-04-10 11:13:23 +0200

    fix: add SSR for now

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,11 @@
 import { defineConfig } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: cloudflare({
+    mode: "directory",
+  }),
+});
