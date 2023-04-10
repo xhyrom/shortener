71e5be12bad93216794a727b23f3a73b27736c06
xHyroM <generalkubo@gmail.com>
2023-04-10 09:59:37 +0200

    init

---
+++ astro.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "astro/config";
+
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  adapter: cloudflare({
+    mode: "directory",
+  }),
+});
