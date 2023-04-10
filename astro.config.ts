b88c30c6a100eafcd0e7b5c5059caa754977e2d9
xHyroM <generalkubo@gmail.com>
2023-04-10 10:03:51 +0200

    fix: add output server

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   adapter: cloudflare({
     mode: "directory",
   }),
