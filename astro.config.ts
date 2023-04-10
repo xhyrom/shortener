2e066ef108900c3d1540e1aaf2b9433e631cf778
xHyroM <generalkubo@gmail.com>
2023-04-10 10:30:22 +0200

    fix: functions

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,6 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/",
   output: "server",
   adapter: cloudflare({
     mode: "directory",
