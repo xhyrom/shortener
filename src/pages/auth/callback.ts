c565009003e5319e54e690b6681c83f886742ffc
xHyroM <github@xhyrom.dev>
2023-04-15 11:49:04 +0200

    fix: make error handling

--- src/pages/auth/callback.ts
+++ src/pages/auth/callback.ts
@@ -7,42 +7,50 @@ import { generateJwt } from "~/lib/jwt";
 export const get: APIRoute = async ({ request }) => {
   const runtime = getRuntime(request).env as CloudflareEnv;
 
-  const { user } = await github.users({
-    options: {
-      clientId: runtime.GITHUB_CLIENT_ID,
-      clientSecret: runtime.GITHUB_CLIENT_SECRET,
-    },
-    request,
-  });
+  try {
+    const { user } = await github.users({
+      options: {
+        clientId: runtime.GITHUB_CLIENT_ID,
+        clientSecret: runtime.GITHUB_CLIENT_SECRET,
+      },
+      request,
+    });
 
-  const exist = await getAccount(runtime.shortener_database, user.id);
-  if (exist) {
-    await updateAccount(
-      runtime.shortener_database,
-      user.name,
-      user.id,
-      user.avatar_url
-    );
-  } else {
-    await createAccount(
-      runtime.shortener_database,
-      user.name,
-      user.id,
-      user.avatar_url,
-      ""
-    );
+    const exist = await getAccount(runtime.shortener_database, user.id);
+    if (exist) {
+      await updateAccount(
+        runtime.shortener_database,
+        user.name,
+        user.id,
+        user.avatar_url
+      );
+    } else {
+      await createAccount(
+        runtime.shortener_database,
+        user.name,
+        user.id,
+        user.avatar_url,
+        ""
+      );
+    }
+
+    const jwt = await generateJwt(runtime.JWT_SECRET, user);
+
+    const now = new Date();
+    now.setTime(now.getTime() + 24 * 3600 * 1000);
+
+    return new Response(null, {
+      status: 302,
+      headers: {
+        location: "/dash",
+        "Set-Cookie": `__hyroshortener-auth=${jwt}; expires=${now.toUTCString()}; path=/;`,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+
+    return new Response(JSON.stringify(error), {
+      status: 500,
+    });
   }
-
-  const jwt = await generateJwt(runtime.JWT_SECRET, user);
-
-  const now = new Date();
-  now.setTime(now.getTime() + 24 * 3600 * 1000);
-
-  return new Response(null, {
-    status: 302,
-    headers: {
-      location: "/dash",
-      "Set-Cookie": `__hyroshortener-auth=${jwt}; expires=${now.toUTCString()}; path=/;`,
-    },
-  });
 };
