cf9b79849e936528a02e6a977d5422e8f8f9b036
xHyroM <github@xhyrom.dev>
2023-05-15 20:09:00 +0200

    feat: navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,7 @@ module.exports = {
         dark: "#111111",
         "dark-50": "#3D3D3D",
         "dark-100": "#2D2D2D",
+        "dark-300": "#252525",
         "dark-400": "#1F1F1F",
         yellow: "#fbc11a",
       },
