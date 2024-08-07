138b167530c61437e4ea5c88446a53b146d7442c
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-08-07 15:12:40 +0200

    build: update deps

--- astro.config.ts
+++ astro.config.ts
@@ -4,43 +4,45 @@ import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
-import compress from "astro-compress";
+import compress from "@playform/compress";
 import tailwind from "@astrojs/tailwind";
 import { CONFIG } from "./src/config";
 import svelte from "@astrojs/svelte";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare({
-    mode: "directory"
+    mode: "directory",
   }),
   site: CONFIG.origin,
   base: "/",
   integrations: [
-  sitemap(),
-  robotsTxt({
-    policy: [
-      {
-        userAgent: "*",
-      },
-    ],
-    sitemap: true,
-  }),
-  compress({
-    css: true,
-    html: true,
-    img: true,
-    js: true,
-    svg: true
-  }), tailwind(), svelte()],
+    sitemap(),
+    robotsTxt({
+      policy: [
+        {
+          userAgent: "*",
+        },
+      ],
+      sitemap: true,
+    }),
+    compress({
+      CSS: true,
+      HTML: true,
+      Image: true,
+      JavaScript: true,
+      SVG: true,
+    }),
+    tailwind(),
+    svelte(),
+  ],
   vite: {
     resolve: {
       alias: {
-        "~": resolve(__dirname, "./src")
-      }
-    }
-  }
+        "~": resolve(__dirname, "./src"),
+      },
+    },
+  },
 });
